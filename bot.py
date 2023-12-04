d46ff59d36984dc762e2ea3ba1f3b84f9830e7c2
Anang11042000 <72434392+Anang11042000@users.noreply.github.com>
2023-12-04 13:23:50 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 from telegram import Bot
 from telegram.ext import CommandHandler, Updater, MessageHandler, Filters, CallbackQueryHandler
 
-GITHUB_TOKEN = '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'
+GITHUB_TOKEN = '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'
 TELEGRAM_BOT_TOKEN = '6363527340:AAGWoPQnlwXkO2RQvAQd0iGQHgprAH3a8_4'
 REPO_OWNER = 'anang11042000'
 REPO_NAME = 'vps'
